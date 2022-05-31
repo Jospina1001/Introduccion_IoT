456b2346537b3a66720bc8ecd4235a1babef855f
Juan <jospina1001@gmail.com>
2022-05-30 19:45:15 -0500

    Union de Codigos

--- Proyecto/get_MQTT_data.py
+++ Proyecto/get_MQTT_data.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import sqlite3
+from azure.iot.device import IoTHubDeviceClient, Message
 from time import time
 
 MQTT_ADDRESS = '192.168.34.38'
@@ -8,13 +9,41 @@ MQTT_PASSWORD = '1234'
 MQTT_TOPIC = 'data/+/+'
 
 DATABASE_FILE = 'data.db'
+CONNECTION_STRING = "HostName=AzureGateway.azure-devices.net;DeviceId=raspberrypi4;SharedAccessKey=hExuwQJqk/zrTomL7+ZhwYn6SSAfWAEM3nUwuDGfYNo="
+
+MSG_TXT = '{{"Temperatura": {temp},"Humedad": {humedad},"Altitud": {altitud},"Presion": {presion}}}'
 
 count=0
 inf1=''
 inf2=''
 inf3=''
 inf4=''
-#prueba de commit
+#Raspberry to Azure
+def iothub_client_telemetry_sample_run():
+    global inf1
+    global inf2
+    global inf3
+    global inf4
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+
+        msg_txt_formatted = MSG_TXT.format(temp=inf1,humedad=inf2,altitud=inf3,presion=inf4)
+        message = Message(msg_txt_formatted)
+
+        # Send the message.
+        print( "Sending message: {}".format(message) )
+        client.send_message(message)
+        print ( "Message successfully sent" )
+
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
 
 def on_connect(client, userdata, flags, rc):
     """ The callback for when the client receives a CONNACK response from the server."""
@@ -86,9 +115,12 @@ def main():
     mqtt_client.on_message = on_message
 
     mqtt_client.connect(MQTT_ADDRESS, 1883)
+    iothub_client_telemetry_sample_run()
+
     mqtt_client.loop_forever()
 
 
 if __name__ == '__main__':
     print('INICIO')
+    print ( "Press Ctrl-C to exit" )
     main()
