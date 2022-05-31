f38758d68b358b96dc42aacb23fe1b1b5cd93b2f
Juan <jospina1001@gmail.com>
2022-05-31 11:20:14 -0500

    Correccion de errores MQTT

--- Proyecto/get_MQTT_data.py
+++ Proyecto/get_MQTT_data.py
@@ -14,6 +14,7 @@ CONNECTION_STRING = "HostName=AzureGateway.azure-devices.net;DeviceId=raspberryp
 MSG_TXT = '{{"Temperatura": {temp},"Humedad": {humedad},"Altitud": {altitud},"Presion": {presion}}}'
 
 count=0
+count2=0
 inf1=''
 inf2=''
 inf3=''
@@ -24,17 +25,21 @@ def iothub_client_telemetry_sample_run():
     global inf2
     global inf3
     global inf4
+    global count2
+    global cliente
+
+    if(count2==0):
+        cliente = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+        count2=1
 
     try:
-        client = iothub_client_init()
-        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
-
         msg_txt_formatted = MSG_TXT.format(temp=inf1,humedad=inf2,altitud=inf3,presion=inf4)
         message = Message(msg_txt_formatted)
 
         # Send the message.
         print( "Sending message: {}".format(message) )
-        client.send_message(message)
+        cliente.send_message(message)
         print ( "Message successfully sent" )
 
     except KeyboardInterrupt:
@@ -92,6 +97,8 @@ def on_message(client, userdata, msg):
          cursor.close()
          count=0
 
+         iothub_client_telemetry_sample_run()
+
 def main():
 
     db_conn = sqlite3.connect(DATABASE_FILE)
@@ -115,7 +122,6 @@ def main():
     mqtt_client.on_message = on_message
 
     mqtt_client.connect(MQTT_ADDRESS, 1883)
-    iothub_client_telemetry_sample_run()
 
     mqtt_client.loop_forever()
 
